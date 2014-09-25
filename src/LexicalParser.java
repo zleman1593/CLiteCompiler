cc39193583fbb04052bdf650933d457094ba0cbe
Zackery Leman <zleman1593@gmail.com>
2014-09-25 08:05:26 -0400

    made file outPut Better

--- src/LexicalParser.java
+++ src/LexicalParser.java
@@ -2,6 +2,7 @@
  */
 
 //TODO: What should I so about the single quotations for char literals?
+//TODO: Make output save in same directory  as input.
 import java.util.ArrayList;
 import java.io.FileInputStream;
 import java.util.Map;
@@ -10,8 +11,11 @@ import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
-
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.File;
 
 public class LexicalParser {
 	//Stores the classified Tokens
@@ -21,8 +25,10 @@ public class LexicalParser {
 	//Holds all the single line comments in the input file
 	private ArrayList<String> comments = new ArrayList<String>();
 	private String rawString;
+	private String fileDirectory;
 	/*Sets up dictionary, reads in file to parse, and calls parsing methods*/
 	public void start(String path) {
+		fileDirectory = path;
 		//Adds Tokens and definitions to a dictionary
 		setupDictionary();
 		//Read the .c file
@@ -34,10 +40,7 @@ public class LexicalParser {
 		//Looks at each lexeme and classifies it into the appropriate Token
 		this.tokens = classifyTokens(extractTokens());
 		//Print the lexeme classification to the console
-		print();
 	}
-
-
 	/*
 	 * This method identifies all the individual lexemes and returns the lexemes
 	 */
@@ -53,13 +56,11 @@ public class LexicalParser {
 		//Removes the comments from the documents and inserts a placeholder token in their place 
 		String commentReplacedString = tabReplacedString.replaceAll("\\//.*\\n", " #comment\n ");
 		//Removes the newline symbol from the document before further parsing 
-		String goodSpacing2 = commentReplacedString.replaceAll("\\n *", " ");
-		//These lines take care of multiple spaces
-		String goodSpacing1 = addSpaces(goodSpacing2).replaceAll("    ", " ");
-		String goodSpacing = goodSpacing1.replaceAll("   ", " ");
-		String allLines = goodSpacing.replaceAll("  ", " ");
+		String newLineReplacedString = commentReplacedString.replaceAll("\\n *", " ");
+		//This line take care of multiple spaces
+		String spacedString = addSpaces(newLineReplacedString).replaceAll("    ", " ").replaceAll("   ", " ").replaceAll("  ", " ");
 		//Divides the document into individual lexemes based on spaces between lexemes
-		String[] tokens = allLines.split(" ");
+		String[] tokens = spacedString.split(" ");
 		return tokens;
 	}
 
@@ -70,7 +71,6 @@ public class LexicalParser {
 		//For all lexemes
 		for(int i = 0; i < tokens.length; i++){
 			ArrayList<String> tempAddTokenAndClassifier = new ArrayList<String>();
-			//Takes care of case when there is no space between last lexeme in the line and ending semicolon
 				/*If the token type doesn't match one in the dictionary parse it to identify
 				what Token it is by which pattern it matches given the remaining possibilities*/
 				String classifier = (String) tokenDef.get(tokens[i]);
@@ -85,13 +85,12 @@ public class LexicalParser {
 					//Strips away ''
 					tokens[i] = tokens[i].substring(1, 2);
 				}
+				
 				tempAddTokenAndClassifier.add(classifier);
 				tempAddTokenAndClassifier.add(tokens[i]);
 				finishedTokens.add(tempAddTokenAndClassifier);
-
 		}
 		return finishedTokens;
-
 	}
 
 	/*
@@ -122,25 +121,46 @@ public class LexicalParser {
 		return "";
 	}
 
-	/*Utility method to print the results of lexeme Token classification to the console*/
-	private void print()
-	{
+	/*Utility method to print the results of lexeme Token classification to the console an save them to a file*/
+	public void print() throws IOException{
+		
+		Path outPutPath = Paths.get(fileDirectory);
+		String outPut = outPutPath.getParent().toString();
+		System.out.println("Output saved in: " + outPut);
+		BufferedWriter outputWriter = null;
+		File file = new File(outPut+"/LexicalOutput.txt");
+		// If file does not exists, then create it.
+		if (!file.exists()) {
+				file.createNewFile();
+		}
+		outputWriter = new BufferedWriter(new FileWriter(file.getAbsoluteFile()));
+	
 		System.out.printf("Tokens          Lexemes\n");
+		outputWriter.write("Tokens          Lexemes\n");
+		outputWriter.write("\n");
 		System.out.printf("\n");
 		String spaces = " ";
-		for (int i = 0; i < tokens.size(); ++i)
-		{	
+		for (int i = 0; i < tokens.size(); ++i){	
 			for( int j = 0; j <= ( 10 - tokens.get(i).get(0).length()); j++){
 				spaces += ' ';	
 			}
 			
-			System.out.printf(" %s %s  %s\n",
-					tokens.get(i).get(0), spaces, tokens.get(i).get(1)); 
-			 spaces = " ";
+			outputWriter.write(tokens.get(i).get(0) + spaces +tokens.get(i).get(1));
+			outputWriter.newLine();
+			System.out.printf(" %s %s%s\n", tokens.get(i).get(0), spaces, tokens.get(i).get(1)); 
+			spaces = " ";
+		}
+		outputWriter.flush();
+		outputWriter.close();
+		
+	}
+	
+	/*Utility method to print the results of  Tokens in a line*/
+	public void printLine(){
+		for (int i = 0; i < tokens.size(); ++i){	
+			System.out.printf(" %s ", tokens.get(i).get(0)); 
 		}
 	}
-
-
 	/*
 	 *Sets up the dictionary that holds most of the lexeme 
 	 * Token classification information.
@@ -180,7 +200,6 @@ public class LexicalParser {
 		tokenDef.put("print","print");
 		tokenDef.put("#comment","comment");
 	}
-	
 	/*This finds all lexemes that may be adjoined to a neighboring lexeme
 	 * and adds a space in between them so that the rest of the parsing can work*/
 	private String addSpaces(String goodSpacing){
@@ -212,20 +231,3 @@ public class LexicalParser {
 		return goodSpacing;
 	}
 }
-
-
-//There is no benefit to including these
-		/*spaceBuffer.add("//comment");
-		 spaceBuffer.add("true");
-		spaceBuffer.add("false");
-		spaceBuffer.add("if");
-		spaceBuffer.add("else");
-		spaceBuffer.add("while");
-		spaceBuffer.add("return");
-		spaceBuffer.add("print");
-		spaceBufferBefore.add("int");
-		spaceBuffer.add("main");
-		spaceBuffer.add("bool");
-		spaceBuffer.add("char");	  
-		spaceBuffer.add("float");*/
-		
--- src/Program.java
+++ src/Program.java
@@ -1,24 +1,27 @@
 /* C-Lite Compiler written  by Zackery Leman 2014
-*/
+ */
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Program {
-	
-public static void main(String[] args)
-{
-	LexicalParser parser  =  new LexicalParser();
-	long start = System.nanoTime();
-	//Can either take a command line argument or use direct input in source code
-	if(args.length == 0){
-		//Defaults here if no command line argument is provided
-		parser.start("/Users/zackleman/Downloads/input.c");
-	} else{
-		parser.start(args[0]);
+
+	public static void main(String[] args) throws IOException
+	{
+		LexicalParser parser  =  new LexicalParser();
+		long start = System.nanoTime();
+		//Can either take a command line argument or use direct input in source code
+		if(args.length == 0){
+			//Defaults here if no command line argument is provided
+			parser.start("/Users/zackleman/Downloads/input.c");
+		} else{
+			parser.start(args[0]);
+		}
+		long runTime = System.nanoTime() - start;
+		System.out.println("Running Time: " + runTime/1000000 + " ms");
+		parser.print();
+		parser.printLine();
+		//ArrayList<ArrayList<String>> tokens = parser.tokens;
 	}
-	long runTime = System.nanoTime() - start;
-	System.out.print("Running Time: " + runTime/1000000 + " ms");
-	//ArrayList<ArrayList<String>> tokens = parser.tokens;
-}
 
 }
